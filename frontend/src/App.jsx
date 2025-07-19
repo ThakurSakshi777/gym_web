0aa0c839ecaa670fc3522324c463b8e14ade58dd
sakshi singh <thakursakshisingh8318@gmail.com>
2025-07-19 18:09:54 +0530

    add initial files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,28 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
+import Hero from './components/Hero.jsx';
+import WorkoutSessions from './components/WorkoutSessions.jsx';
+import Gallery from './components/Gallery.jsx';
+import Pricing from './components/Pricing.jsx';import Contact from './components/Contact.jsx';
+import BMICalculator from './components/BMICalculator.jsx';
+
 
 
 function App() {
   return (
     <Router>
-    
+      <Navbar />
+      <Hero />
+      <WorkoutSessions />
+      <Gallery />
+      <Pricing />
+      <Contact />
+      <BMICalculator />
+      <Footer/>
+      <ToastContainer theme='drak' position='top-right' autoClose={3000} hideProgressBar={false} closeOnClick pauseOnHover draggable pauseOnFocusLoss />
     </Router>
   )
 }
---
+++ frontend/src/components/BMICalculator.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const BMICalculator = () => {
+  return (
+    <div>BMICalculator</div>
+  )
+}
+
+export default BMICalculator
+
