382bbba8098827ff9f30d428fbabe31e270c3303
sakshi singh <thakursakshisingh8318@gmail.com>
2025-07-27 20:19:59 +0530

    add routers

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,58 @@
 import express from 'express';
 import { config } from 'dotenv';
 import cors from 'cors';
+import { sendEmail } from './utils/sendEmail.js';
+
+config({ path: './config.env' });
 
 const app = express();
 const router = express.Router();
 
-config({ path: './config.env' });
+
 
 // console.log('Environment Variables:', process.env.PORT);
 
 app.use(cors({
     origin: [process.env.FRONTEND_URL],
-    methods: ['GET'],
+    methods: ['GET','POST'],
     credentials: true
 }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-router.get('/', (req, res ,next) => {
-    res.json({ success:true , message: "hello network"});
-}) ;
+router.post("/send/mail" , async (req , res , next)=>{
+ const { name ,email , message} = req.body;
+ if ( !name || !email || !message) {
+    return next (
+        res.status(400).json ({
+        success: false ,
+        message: "please proivide all details"
+    })
+    );
+  }
+  try {
+    await  sendEmail({
+      email: "thakursakshisingh8318@gmail.com",
+      subject: "GYM_WEB cONTACT",
+      message,
+      userEmail: email,
+    });
+    res.status(200).json({
+      success: true,
+      message: "Email sent successfully",
+    })
+    
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to send email",
+      
+    });
+    
+  }
+ 
+});
 
 app.use(router);
  
--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { useState } from 'react';
+import {axios} from "axios";
+import {toast} from "react-toastify"
+import { sendEmail } from '../../../backend/utils/sendEmail';
 
 const Contact = () => {
   const[name , setName] = useState("");
@@ -7,10 +10,38 @@ const Contact = () => {
   const[message , setMessage] = useState("");
   const[loading , setLoading] = useState(false);
 
+  const SendMail = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const {data} = await axios.post(
+        "http://localhost:4000/send/mail",
+        {
+          name,
+          email,
+          message,
+        } ,
+        {
+          withCredentials:true,
+          headers:{"content-Type": "appliction/json"},
+        }
+      );
+      setName("");
+      setEmail("");
+      setMessage("");
+      toast.success(data.message);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      toast.error(error.response.data.message)
+    }
+  };
+
   return (
     <section className='contact'>
 
-      <form action="">
+      <form onSubmit={sendEmail}>
          <h1>CONTACT US</h1>
          <div>
           <label> Name </label>
